a48a01871b158714aca77ce830d80118d0bcfebb
tracerout <tracerout@yahoo.com>
2018-06-19 19:48:47 +0200

    shell base

--- shell/shell.go
+++ shell/shell.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/manifoldco/promptui"
+)
 
+func main() {
+	prompt := promptui.Select {
+		Label: "Select Day",
+		Items: []string{"Enter random pod from deployment", "Create a pod for me"},
+		Templates: &promptui.SelectTemplates{
+			Active: `▸ {{ . }}`, // gotemplate
+			Inactive: `{{ . }}`,
+		},
+	}
+
+	_, result, err := prompt.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	fmt.Printf("You choose %q\n", result)
 }
